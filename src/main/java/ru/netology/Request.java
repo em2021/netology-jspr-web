01615ca30a40afaf861b45ca28ef3997b3a7e9ac
em2021 <egor_m@icloud.com>
2023-03-06 22:01:52 +0300

    Query params processing feature added

--- src/main/java/ru/netology/Request.java
+++ src/main/java/ru/netology/Request.java
@@ -1,17 +1,26 @@
 package ru.netology;
 
+import org.apache.hc.client5.http.classic.HttpClient;
+import org.apache.hc.core5.http.NameValuePair;
+import org.apache.hc.core5.net.URLEncodedUtils;
+
+import java.nio.charset.Charset;
+import java.util.List;
+
 public class Request {
 
     private final String method;
     private final String path;
+    private final String params;
     private final String headers;
     private final String body;
 
-    public Request(String method, String path, String headers, String body) {
+    public Request(String method, String path, String params, String headers, String body) {
         this.method = method;
         this.path = path;
         this.headers = headers;
         this.body = body;
+        this.params = params;
     }
 
     public String getMethod() {
@@ -20,6 +29,21 @@ public class Request {
 
     public String getPath() {
         return path;
+    }
+
+    public List <NameValuePair> getQueryParams() {
+        if (params == null) {
+            return null;
+        }
+        return URLEncodedUtils.parse(params, Charset.defaultCharset());
+    }
+
+    public NameValuePair getQueryParam(String name) {
+        if (params == null) {
+            return null;
+        }
+        List<NameValuePair> queryParams = getQueryParams();
+        return queryParams.get(queryParams.indexOf(name));
     }
 
     public String getHeaders() {
